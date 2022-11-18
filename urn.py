056cabe8c6e75cef453f45f4e56934a533eeda15
FluffyCookie000 <fluffycookiecat000@gmail.com>
2022-11-18 17:39:20 -0500

    urn

--- urn.py
+++ urn.py
@@ -7,7 +7,7 @@ import discord
 urn = ["urn", "Urn", "URn", "URN", "urN", "uRN", "uRn", "UrN",]
 
 load_dotenv()
-bot = commands.Bot(command_prefix=['1043241661179900074 '], intents=discord.Intents.all(), help_command=None)
+bot = commands.Bot(command_prefix=['<@1043241661179900074> '], intents=discord.Intents.all(), help_command=None)
 bot.startTime = DT.now()
 bot.currentTime = DT.now()
 
@@ -17,19 +17,19 @@ async def on_ready():
     await bot.change_presence(activity=discord.Game(name="urn"))
 
 @bot.command()
-async def kys():
-    await bot.close()
+async def kys(ctx):
+    if ctx.author.id == 557286947106586627:
+        await bot.close()
 
 @bot.event
 async def on_message(message):
-    if message.content == "urn" or message.content == "Urn" or message.content == "URn" or message.content == "URn" or message.content == "URN" or message.content == "urN" or message.content == "uRN" or message.content == "uRn" or message.content ==  "UrN":
-        await message.reply("⚱")
-    elif "u" in message.content or "U" in message.content:
-        if "r" in message.content or "R" in message.content:
-            if "n" in message.content or "N" in message.content:
-                await message.add_reaction("⚱")
-
-
-                  
+    if message.author.id != 1043241661179900074:
+        if "⚱" in message.content:
+            await message.add_reaction("⚱")
+        elif "u" in message.content or "U" in message.content:
+            if "r" in message.content or "R" in message.content:
+                if "n" in message.content or "N" in message.content:
+                    await message.add_reaction("⚱")
+        await bot.process_commands(message) 
 
 bot.run(os.getenv('TOKEN'))
